2a3045e0c70a016ad00fc6cd3f719728d5a5bf29
Tran Kevin <trankevinmay@gmail.com>
2022-03-11 13:15:36 +0100

    reload feature

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,81 @@
-// Je crée un mot
+// array de mot
 const arrWords = ["rat", "attribution", "famille", "robe", "autruche", "entropie", "moustache", "arrondi", "compliment", "chaleureux", "refuser", "fatiguer", "intriguer", "interroger", "parcourir", "travailler", "jouer", "ordinateur", "poubelle", "chanceux", "amour", "comptable", "orgasme", "zoologie", "dinosaure", "constante", "courant", "contre", "couleur", "cadre", "camion", "corne", "cadeau", "cochon", "colle", "barbe", "bonbon", "bouton", "bouchon", "ballon", "bille", "alerte", "attention", "amour", "attache", "attaque", "arrondi"];
 // ATTENTION AU ACCENTS  "considération"
+
+// Le mot aléatoire
 let word = arrWords[Math.round(Math.random() * arrWords.length)];
+//console.log(word); // A EFFACER APRES
+
+// if(document.getElementById("list").value == 0){
+//     word = arrWords[Math.round(Math.random() * arrWords.length)];
+// }
 console.log(word);
 
+
+
+// Je récupère la valeur de ma dropdownlist
+// let level;
+
+// document.getElementsByClassName("all")[0].addEventListener('click', () => {
+//     word = arrWords[Math.round(Math.random() * arrWords.length)];
+// });
+// document.getElementsByClassName("easy")[0].addEventListener('click', () => {
+//     while (letter > 7) {
+//         word = arrWords[Math.round(Math.random() * arrWords.length)];
+//     }
+//     console.log("rat");
+// });
+// document.getElementsByClassName("normal")[0].addEventListener('click', () => {
+//     while (letter > 7 && letter < 9) {
+//         word = arrWords[Math.round(Math.random() * arrWords.length)];
+//     }
+// });
+// document.getElementsByClassName("hard")[0].addEventListener('click', () => {
+//     while (letter <= 9) {
+//         word = arrWords[Math.round(Math.random() * arrWords.length)];
+//     }
+// });
+// console.log(word);
+// let letter = word.length;
+// console.log(letter);
+
+// document.getElementById("list").addEventListener('click', () => {
+//     level = document.getElementById("list").value;
+//     console.log(level);
+//     switch (document.getElementById("list").value) {
+//         case "0":
+//             word = arrWords[Math.round(Math.random() * arrWords.length)];
+//             console.log(word);
+//             console.log(letter);
+//             break;
+//         case "1":
+//             while (letter > 7) {
+//                 word = arrWords[Math.round(Math.random() * arrWords.length)];
+//             }
+//             console.log(word);
+//             console.log(letter);
+//             break;
+//         case "2": while (letter > 7 && letter < 9) {
+//             word = arrWords[Math.round(Math.random() * arrWords.length)];
+//         }
+//             console.log(word);
+//             console.log(letter);
+//             break;
+//         case "3":
+//             while (letter <= 9) {
+//                 word = arrWords[Math.round(Math.random() * arrWords.length)];
+//             }
+//             console.log(word);
+//             console.log(letter);
+//             break;
+//     }
+// });
+
+
+
+
 // Tableau d'image 
-const arrImage = ["assets/images/Scene0.png","assets/images/Scene1.png", "assets/images/Scene2.png","assets/images/Scene3.png", "assets/images/Scene4.png","assets/images/Scene5.png", "assets/images/Scene6.png"];
+const arrImage = ["assets/images/Scene0.png", "assets/images/Scene1.png", "assets/images/Scene2.png", "assets/images/Scene3.png", "assets/images/Scene4.png", "assets/images/Scene5.png", "assets/images/Scene6.png"];
 // Variable vie
 let life = 5;
 
@@ -49,8 +119,8 @@ document.addEventListener("keyup", (event) => {
                 win++;
                 if (win == word.length) {
                     document.getElementsByTagName("img")[0].src = imgVictoire[0];
-                    console.log("Victoire!")
-                    // CHOISIR UNE IMAGE POUR LA VICTOIRE!
+                    document.getElementById("input").disabled = true;
+                    reload();
                 }
             }
         }
@@ -61,10 +131,22 @@ document.addEventListener("keyup", (event) => {
             document.getElementsByTagName("img")[0].src = arrImage[lose];
             if (lose == life + 1) {
                 document.getElementById("input").disabled = true;
-                console.log("Perdu!");
+                reload();
             }
         }
         document.getElementById("input").value = "";
     }
     // ATTENTION PROBLEME VICTOIRE QUAND ON ECRIS LA MEME LETTRE
 });
+
+function reload() {
+    let btnReload = document.createElement("button");
+    btnReload.setAttribute("type", "button");
+    btnReload.setAttribute("name", "reload");
+    btnReload.setAttribute("id", "reload");
+    document.getElementsByClassName("inputFlex")[0].appendChild(btnReload);
+    btnReload.innerHTML = "encore?";
+    document.getElementById("reload").addEventListener('click', ()=>{
+        location.reload();
+    });
+}
